b4009b2975797cd8ed55bb2995b6320b87d445a6
Mihaf AJunaidel <maljunaydil@taqeem.gov.sa>
2020-10-29 13:01:20 +0300

    google calendar configation

---
+++ Calendat.js
@@ -0,0 +1,9 @@
+// const calendarKey= "171649701961-784pm7ht88p5je0rig58ebdg2nolv11v.apps.googleusercontent.com";
+const secretKey = "OwmDR4OOhO6xSvDHVN0-lMBc";
+//  const  apiKey = "AIzaSyABhJOK48o3tsw_ec3R8jEJBnsCsteHrYc";
+ {
+    "clientId"= "646973144767-ull5rr2noc3rghn9gbic5ekkrucdj8co.apps.googleusercontent.com",
+    "apiKey"= "AIzaSyABhJOK48o3tsw_ec3R8jEJBnsCsteHrYc",
+    "scope"= "https://www.googleapis.com/auth/calendar",
+    "discoveryDocs" =["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
+  }
