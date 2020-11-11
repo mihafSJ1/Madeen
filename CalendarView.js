b168b09171912e58e835c9a1776614107ca7410a
Hessah20 <60852126+Hessah20@users.noreply.github.com>
2020-11-11 19:27:01 +0300

    Revert "Merge branch 'master' of https://github.com/mihafSJ1/Madeen"
    This reverts commit dc2ac753287019c9f1b29bb0921f3f4880c70cdd, reversing
    changes made to 8cc69fa335c130a2d3ac670ca71b2f5108150d92.

--- CalendarView.js
+++ CalendarView.js
@@ -106,7 +106,7 @@ firebase
                 });
           }
         }} 
- });//comment 1
+ });
       
   });
   for (var i =0 ;i< onceRequests.length;i++){
@@ -143,7 +143,7 @@ firebase
 
             
               //     })
-         //comment 1
+         
              }
 
     }
@@ -168,7 +168,7 @@ firebase
           //   flag: false,
           //     })
          
-     }//comment 1
+     }
     }
   if (installmentRequests[i].installmentsType == "أسبوعيًا"){
 
@@ -188,7 +188,7 @@ firebase
               //   installemntPrice: installmentRequests[i].installemntPrice,
               //   key: installmentRequests[i].key,
               //   price :  installmentRequests[i].price
-              //     })//comment 1
+              //     })
      }
     }
     if(installmentRequests[i].installmentsType == "يوميًا"){
@@ -214,7 +214,7 @@ firebase
             
      }
   
-   //comment 1
+   
     }
 
     
@@ -245,7 +245,7 @@ firebase
 
 
 
-//comment 1
+
 
 
   render() {
@@ -266,7 +266,7 @@ firebase
       }
 
  
-      //comment 1
+      
 
  
      
@@ -280,9 +280,8 @@ firebase
 
      
        theme={{
-       //comment 1 
-      //comment 1
-      //comment 1
+        
+      
     todayTextColor: '#57694C',
     dayTextColor: '#2d4150', // لون أرقام أيام التاريخ ا
     selectedColor: '#CBCA9E',
@@ -327,7 +326,7 @@ firebase
      
     setTimeout(() => {
 
-   //comment 1
+   
    
       
 for (let i =0 ;i<array.length;i++){
@@ -356,7 +355,7 @@ if(!array[i].flag){
 }
 
          
-       //comment 1   
+          
 // for (var i = 0; i< onceRequests.length;i++){
 //   if( !this.state.items[onceRequests[i].expectedDate] ){
 //     this.state.items[onceRequests[i].expectedDate] = [];
@@ -369,12 +368,12 @@ if(!array[i].flag){
 //               borderBottomColor:"#D9AE94",
 //               borderRadius: 10,
            
-           //comment 1   
+              
              
 //            });
 //   }
 //           }
-      //comment 1
+      
       for (var i = -15; i < 85; i++) {
         const time = day.timestamp + i * 24 * 60 * 60 * 1000;
         const strTime = this.timeToString(time);
@@ -397,7 +396,7 @@ if(!array[i].flag){
 //  console.log(onceRequests)
 //  console.log("ins")
 //  console.log(dates)
- //comment 1
+ 
     // }
       const newItems = {};
       Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
@@ -407,7 +406,7 @@ if(!array[i].flag){
     
     }, 1000);
 
-  //comment 1
+  
   }
   renderItem(item) {
     {item.amount == null ?item.height= 100
@@ -437,7 +436,7 @@ if(!array[i].flag){
   );
 }
 }  
-  //comment 1
+  
   }
 
   renderEmptyDate() {
@@ -450,7 +449,7 @@ if(!array[i].flag){
   rowHasChanged(r1, r2) {
     return r1.name !== r2.name;
   }
-//comment 1
+
   timeToString(time) {
   
     const date = new Date(time);
@@ -461,7 +460,7 @@ const styles = StyleSheet.create({
     container:{
 marginTop:50,
 marginBottom:0,
-//comment 1
+
 
     },
 
@@ -479,13 +478,12 @@ marginHorizontal:5,
     borderWidth:0.5,
     //backgroundColor:'red',
     fontFamily:'Bahij_TheSansArabic-Light',
-    //comment 1
   },
   emptyDate: {
     height: 15,
     flex:1,
     paddingTop: 30,
-    //comment 1
+    
   //  backgroundColor:'red',
   },
   textCard:{
@@ -495,7 +493,7 @@ marginHorizontal:5,
     fontSize:15,
     color:'#746356',
     bottom:5,
-  //comment 1
+  
 
   }
 });
